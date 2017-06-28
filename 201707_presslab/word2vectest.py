c537e26f33fa5e7ce46c0edb4e7059726abfc150
linasugi <sogenakotoga@hotmail.com>
2017-06-28 23:23:45 +0900

    mecab install

---
+++ 201707_presslab/word2vectest.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 28 22:27:24 2017
+
+
+@author: kazuyuki
+"""
+
+import MeCab
+
+mecab = MeCab.Tagger("-Ochasen")
+
+mecab = MeCab.Tagger("mecabrc")
